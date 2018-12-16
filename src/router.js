94a29b8bb5c5c371c42c4eb88eb2bee7da977cdb
Anton Reshetov <reshetov.art@gmail.com>
2018-12-16 21:40:11 +0300

    add: basic route

--- src/router.js
+++ src/router.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import DefaultTheme from './components/layouts/Default'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: []
+  routes: [
+    {
+      path: '/',
+      component: DefaultTheme
+    }
+  ]
 })
