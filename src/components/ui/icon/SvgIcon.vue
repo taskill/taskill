4e938da0f411d6f921c62cce80412684399c39f9
Anton Reshetov <reshetov.art@gmail.com>
2018-12-17 10:23:11 +0300

    add: svg icon component

---
+++ src/components/ui/icon/SvgIcon.vue
@@ -0,0 +1,43 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="width"
+    :height="height"
+    viewBox="0 0 24 24"
+    v-html="icon"
+  ></svg>
+</template>
+
+<script>
+import icons from './icons.js'
+
+export default {
+  name: 'SvgIcon',
+
+  props: {
+    name: String,
+    width: {
+      type: [String, Number],
+      default: 24
+    },
+    height: {
+      type: [String, Number],
+      default: 24
+    }
+  },
+
+  computed: {
+    icon () {
+      if (icons[this.name]) {
+        return icons[this.name]
+      } else {
+        console.error('Name of icon is not correct')
+        return undefined
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
