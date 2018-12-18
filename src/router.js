a535053fabd33ba87aca6239a0da7c8abd6e4a42
Anton Reshetov <reshetov.art@gmail.com>
2018-12-18 20:48:11 +0300

    feat(router): add active class

--- src/router.js
+++ src/router.js
@@ -7,6 +7,8 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
+  linkActiveClass: 'active',
+  linkExactActiveClass: 'active-exact',
   routes: [
     {
       path: '/',
