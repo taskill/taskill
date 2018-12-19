a724129295eb6899d8ffb5cfedb70a74cff9a35b
Anton Reshetov <reshetov.art@gmail.com>
2018-12-19 08:59:56 +0300

    feat: collect ui components

---
+++ src/components/ui/index.js
@@ -0,0 +1,6 @@
+import SvgIcon from '../ui/icon/SvgIcon'
+import SideNav from '../ui/navigation/SideNav'
+import SideNavItem from '../ui/navigation/SideNavItem'
+import SideNavGroup from '../ui/navigation/SideNavGroup'
+
+export { SvgIcon, SideNav, SideNavItem, SideNavGroup }
