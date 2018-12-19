802908f49df5abbfc24ddac6b33b11077c748b72
Anton Reshetov <reshetov.art@gmail.com>
2018-12-19 08:53:04 +0300

    feat: add app navigation list

---
+++ src/navigation.js
@@ -0,0 +1,14 @@
+export default {
+  sideMenu: [
+    {
+      name: 'Task',
+      icon: 'inbox',
+      path: '/tasks/new'
+    },
+    {
+      name: 'Project',
+      icon: 'briefcase',
+      path: '/projects/new'
+    }
+  ]
+}
