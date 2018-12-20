e4ef5a4a940e00c7a4b4adb29602a729afd615e8
Anton Reshetov <reshetov.art@gmail.com>
2018-12-20 21:27:39 +0300

    feat: add element-ui locale, style & notification into global

--- src/main.js
+++ src/main.js
@@ -2,9 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { Loading, Message, MessageBox, Notification } from 'element-ui'
+import lang from 'element-ui/lib/locale/lang/en'
+import locale from 'element-ui/lib/locale'
+import 'element-ui/lib/theme-chalk/index.css'
 import './assets/scss/style.scss'
 
 Vue.config.productionTip = false
+Vue.use(Loading.directive)
+locale.use(lang)
+
+Vue.prototype.$loading = Loading.service
+Vue.prototype.$msgbox = MessageBox
+Vue.prototype.$alert = MessageBox.alert
+Vue.prototype.$confirm = MessageBox.confirm
+Vue.prototype.$prompt = MessageBox.prompt
+Vue.prototype.$notify = Notification
+Vue.prototype.$message = Message
 
 new Vue({
   router,
