11c9309be5afbfdd517219f81312fffbd2a7f2f8
Anton Reshetov <reshetov.art@gmail.com>
2018-12-21 22:10:43 +0300

    refactor: store with modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import user from './modules/user'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  modules: {
+    user
+  }
 })
