44967fd6c554af0510a1a36bf576cec485801b32
Anton Reshetov <reshetov.art@gmail.com>
2018-12-22 07:38:51 +0300

    feat(router): add middleware for auth checking

--- src/router.js
+++ src/router.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import DefaultTheme from './components/layouts/Default'
+import auth from './services/auth'
+import store from './store'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   linkActiveClass: 'active',
@@ -37,3 +39,25 @@ export default new Router({
     }
   ]
 })
+
+// Middleware
+router.beforeEach(async (to, from, next) => {
+  if (to.meta.requiresAuth) {
+    if (auth.isAuth()) {
+      // Fetch user data from store
+      const user = store.getters.user
+      // If not exist user data fetch from server
+      if (Object.keys(user).length === 0) {
+        const { id } = auth.getUser()
+        await store.dispatch('getUserById', { id })
+      }
+      next()
+    } else {
+      next('/signin')
+    }
+  } else {
+    next()
+  }
+})
+
+export default router
