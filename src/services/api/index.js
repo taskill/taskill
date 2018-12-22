955f86f8bc5aa87d75653f12e9fa760b572ac615
Anton Reshetov <reshetov.art@gmail.com>
2018-12-22 07:39:53 +0300

    feat(api): add get uset by id method

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -4,3 +4,9 @@ export const signIn = body => {
   let url = '/users/signin'
   return http.post(url, body)
 }
+
+export const getUserById = params => {
+  let id = params.id
+  let url = `/users/${id}`
+  return http.get(url)
+}
