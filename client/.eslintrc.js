915cff47331f21f8dd87154058a6a7355a11bead
Anton Reshetov <reshetov.art@gmail.com>
2019-01-12 20:30:05 +0300

    chore(eslint): change vue template rule

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -9,7 +9,8 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/component-name-in-template-casing': ['error', 'kebab-case']
   },
   parserOptions: {
     parser: 'babel-eslint'
