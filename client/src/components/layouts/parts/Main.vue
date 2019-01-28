aff4f0c746e52219f6524b6431ec59b3b583062b
Anton Reshetov <reshetov.art@gmail.com>
2019-01-28 18:00:39 +0300

    feat(layout): add top bar

--- client/src/components/layouts/parts/Main.vue
+++ client/src/components/layouts/parts/Main.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="main">
+    <top-bar />
     <router-view />
   </div>
 </template>
 
 <script>
+import TopBar from './TopBar'
+
 export default {
   name: 'Main',
+
+  components: {
+    TopBar
+  },
 
   data () {
     return {
@@ -17,4 +24,7 @@ export default {
 </script>
 
 <style lang="scss">
+.main {
+  padding: 20px 30px;
+}
 </style>
---
+++ client/src/components/layouts/parts/TopBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="top-bar">
+    <div class="top-bar__title">
+      {{ $route.meta.title }}
+    </div>
+    <div class="top-bar__description">
+      {{ $route.meta.description }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopBar',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.top-bar {
+  margin-bottom: 30px;
+  &__title {
+    font-size: 24px;
+  }
+  &__description {
+    color: $color-grey-2;
+  }
+}
+</style>
