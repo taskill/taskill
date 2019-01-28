aa9226595ae4c1e9002e5c7492fa026f48a3515d
Anton Reshetov <reshetov.art@gmail.com>
2019-01-28 17:45:32 +0300

    chore: import favorite project list

--- client/src/components/ui/index.js
+++ client/src/components/ui/index.js
@@ -2,6 +2,14 @@ import SvgIcon from './icon/SvgIcon'
 import SideNav from './navigation/SideNav'
 import SideNavItem from './navigation/SideNavItem'
 import SideNavGroup from './navigation/SideNavGroup'
+import FavoriteProjectList from './navigation/FavoriteProjectList'
 import Quill from './quill/Quill'
 
-export { SvgIcon, SideNav, SideNavItem, SideNavGroup, Quill }
+export {
+  SvgIcon,
+  SideNav,
+  SideNavItem,
+  SideNavGroup,
+  FavoriteProjectList,
+  Quill
+}
