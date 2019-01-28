67a2fccb75a515f9528a8fe40d3fa663efeb8218
Anton Reshetov <reshetov.art@gmail.com>
2019-01-28 17:42:32 +0300

    feat(utils): add sluggify

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -9,3 +9,23 @@ export const queryToUrl = (url, query) => {
   url = url + '/?' + qs.stringify(query, { arrayFormat: 'repeat' })
   return url
 }
+/**
+ * Convert a string to a slug
+ * @param {string} string - string
+ * @returns {string} sluggified string
+ */
+export const sluggify = (s) => {
+  return s
+    .toLowerCase()
+    .trim()
+    .replace(/:.*:/g, '')
+    .replace(/ +$/g, '')
+    .replace(/(&amp;| & )/g, '-and-')
+    .replace(/&(.+?);/g, '')
+    .replace(/[\s\W-]+/g, '-')
+}
+/**
+ * Acronym generator
+ * @param {string} string - string to acronym
+ * @param {number} maxLetter - max letter acronym
+ */
