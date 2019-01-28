94c8888c7e401b35c44e49247841ef23ea711a2f
Anton Reshetov <reshetov.art@gmail.com>
2019-01-28 17:58:46 +0300

    feat(api): update api

--- client/src/services/api/index.js
+++ client/src/services/api/index.js
@@ -1,5 +1,7 @@
 import http from './config'
+import { queryToUrl } from '../../utils'
 
+// User
 export const signIn = body => {
   let url = '/users/signin'
   return http.post(url, body)
@@ -14,4 +16,45 @@ export const getUserById = params => {
   let id = params.id
   let url = `/users/${id}`
   return http.get(url)
+}
+// Project
+/**
+ * Список проектов
+ */
+export const getProjects = query => {
+  let url = '/projects'
+  if (query) url = queryToUrl(url, query)
+  return http.get(url)
+}
+/**
+ * Список избранных проектов
+ */
+export const getFavoriteProjects = query => {
+  let url = '/projects/favorite'
+  if (query) url = queryToUrl(url, query)
+  return http.get(url)
+}
+/**
+ * Создание проекта
+ */
+export const createProject = body => {
+  let url = '/projects'
+  return http.post(url, body)
+}
+/**
+ * Обновление проекта
+ */
+export const updateProjectById = (params, body) => {
+  let projectId = params.id
+  let url = `/projects/${projectId}`
+  return http.put(url, body)
+}
+
+/**
+ * Переключение проекта в избранное
+ */
+export const toggleProjectFavorite = (params, body) => {
+  let projectId = params.id
+  let url = `/projects/${projectId}/favorite`
+  return http.post(url, body)
 }
