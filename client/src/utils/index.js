240cdd7f0a68ce3567cb88e2de9a1df21e59a455
Anton Reshetov <reshetov.art@gmail.com>
2019-01-28 18:12:22 +0300

    fix: parse float

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -49,9 +49,9 @@ export const contrast = hex => {
 
   hex = hex.replace(/#/, '')
 
-  const r = Number(hex.substr(0, 2), 16)
-  const g = Number(hex.substr(2, 2), 16)
-  const b = Number(hex.substr(4, 2), 16)
+  const r = parseInt(hex.substr(0, 2), 16)
+  const g = parseInt(hex.substr(2, 2), 16)
+  const b = parseInt(hex.substr(4, 2), 16)
 
   const yiq = (r * 299 + g * 587 + b * 114) / 1000
 
