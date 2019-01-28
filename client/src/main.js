45110e5bbc05966515802ff66878d9eb7af3eaf0
Anton Reshetov <reshetov.art@gmail.com>
2019-01-28 08:30:23 +0300

    feat: add global config size of element-ui

--- client/src/main.js
+++ client/src/main.js
@@ -19,6 +19,7 @@ Vue.prototype.$confirm = MessageBox.confirm
 Vue.prototype.$prompt = MessageBox.prompt
 Vue.prototype.$notify = Notification
 Vue.prototype.$message = Message
+Vue.prototype.$ELEMENT = { size: 'small' }
 
 new Vue({
   router,
